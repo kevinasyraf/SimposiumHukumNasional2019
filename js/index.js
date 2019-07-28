24acb3cc358e630a4ced899259013b7fc0f460e3
Reynard Adha Ryanda <reynardryanda@gmail.com>
2019-07-28 21:05:52 +0700

    many changes

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
-// Navbar
-let navbar = document.getElementById("navbar");
+// // Navbar
+// let navbar = document.getElementById("navbar");
 
-// Navbar Items
-let navbarItems = document.querySelector(".navbar-items");
+// // Navbar Items
+// let navbarItems = document.querySelector(".navbar-items");
 
-// // Sticky Navbar
-window.onscroll = stickyNavbar;
+// // // Sticky Navbar
+// window.onscroll = stickyNavbar;
 
-// Only need once
-let sticky = navbar.offsetTop;
-// console.log(sticky + "sticky");
+// // Only need once
+// let sticky = navbar.offsetTop;
+// // console.log(sticky + "sticky");
 
-function stickyNavbar() {
-  //Window.pageYOffset is the page y coord
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky");
-    navbarItems.classList.add("after-sticky");
-  } else {
-    navbar.classList.remove("sticky");
-    navbarItems.classList.remove("after-sticky");
-  }
+// function stickyNavbar() {
+//   //Window.pageYOffset is the page y coord
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//     navbarItems.classList.add("after-sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//     navbarItems.classList.remove("after-sticky");
+//   }
 
-  //   console.log(window.pageYOffset + "page offset y");
-}
+//   //   console.log(window.pageYOffset + "page offset y");
+// }
